cb3a573ddee4624cc23d24fa1fa328e9a1e28567
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-05-16 17:39:10 +0100

    logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -6,7 +6,7 @@ const useLogout = () =>{
     const logout = async () =>{
         setAuth({});
         try {
-            const res = await axios('/logout')
+            const res = await axios.post('/logout')
             console.log(res)
         } catch (error) {
             console.log(error)
